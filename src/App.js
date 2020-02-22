0846eea9d7ddc98a8c24b47831cdcb66cd700bfb
Edvinas Pranka <epranka@gmail.com>
2020-02-22 23:07:34 +0200

    :tada: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [binaryIsValid, setBinaryIsValid] = useState(true);
+  const [binaryValue, setBinaryValue] = useState("");
+  const [decimalValue, setDecimalValue] = useState("");
+
+  const formatValue = value => {
+    return value
+      .replace(/(\d{4})/g, "$1 ")
+      .replace(/\s+/g, " ")
+      .replace(/^\s+|\s+$/g, "");
+  };
+
+  const handleBinaryValueChange = e => {
+    let value = String(e.target.value);
+    if (!value) {
+      setBinaryValue("");
+      setBinaryIsValid(true);
+    } else {
+      setBinaryIsValid(/^([01\s]+)$/.test(value));
+      setBinaryValue(formatValue(value));
+    }
+  };
+
+  const calculateDecimal = binaryString => {
+    let decimalResult = 0;
+    for (
+      let i = 0, j = binaryString.length - 1;
+      i < binaryString.length;
+      i++, j--
+    ) {
+      const digit = parseInt(binaryString[i]);
+      decimalResult += digit * Math.pow(2, j);
+    }
+    return decimalResult;
+  };
+
+  useEffect(() => {
+    if (binaryIsValid && binaryValue) {
+      const decimalValue = calculateDecimal(binaryValue.replace(/\s+/g, ""));
+      setDecimalValue(decimalValue);
+    } else {
+      setDecimalValue("");
+    }
+  }, [binaryValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="bin2dec">
+      <h1 id="header">Bin2Dec</h1>
+      <input
+        id="binary"
+        className={binaryIsValid ? "" : "invalid"}
+        type="text"
+        placeholder="1010 1010"
+        value={binaryValue}
+        onChange={handleBinaryValueChange}
+      />
+      <div id="decimal">{decimalValue}</div>
     </div>
   );
 }
